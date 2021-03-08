46a5329177a9d132b5d65d27676e688c3a0560b1
LoganGerber <logangerber1014@yahoo.com>
2021-03-08 00:46:43 -0800

    Remove SettingTypeError

--- src/backend/settings/setting-type-error.ts
+++
@@ -1,6 +0,0 @@
-export class SettingTypeError extends Error {
-	constructor(settingName: string, providedType: string, providedValue: any, expectedType: string) {
-		super(`SettingTypeError: Setting ${settingName} provided incorrect type ${providedType} (${providedValue}), expected type ${expectedType}`);
-		this.name = "SettingTypeError";
-	}
-}
