237a36025829a41674dd206c12ff96706bdfbee4
LoganGerber <logangerber1014@yahoo.com>
2021-11-28 02:22:52 -0800

    Remove compiled typescript from repo

--- lib/frontend/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const fill: Element;
-declare const sections: NodeListOf<Element>;
-declare function dragStart(e: DragEvent): void;
-declare function dragEnd(e: DragEvent): void;
-declare function dragOver(e: DragEvent): void;
-declare function dragEnter(e: DragEvent): void;
-declare function dragLeave(e: DragEvent): void;
-declare function drop(e: DragEvent): void;
-declare function getTarget(e: Event): Element;
