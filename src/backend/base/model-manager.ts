f1a6dc2390abfee2766e8a267a9af3381998ebe7
LoganGerber <logangerber1014@yahoo.com>
2021-12-23 19:30:45 -0800

    create base ModelManager class

---
+++ src/backend/base/model-manager.ts
@@ -0,0 +1,14 @@
+import { Model } from "./model";
+
+class ModelManager {
+	private _createdModels: Record<string, Model>;
+
+	constructor() {
+		this._createdModels = {};
+	}
+
+	
+}
+
+
+export default new ModelManager();
