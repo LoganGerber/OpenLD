7d95ec4be705706b83c5374760333b5a1c128483
LoganGerber <logangerber1014@yahoo.com>
2022-01-02 23:08:26 -0800

    Add more functionality to ModelManager

--- src/backend/base/model-manager.ts
+++ src/backend/base/model-manager.ts
@@ -1,13 +1,28 @@
-import { Model } from "./model";
+import Model from "./model";
 
 class ModelManager {
-	private _createdModels: Record<string, Model>;
+	private _registeredModels: Record<string, Model>;
 
 	constructor() {
-		this._createdModels = {};
+		this._registeredModels = {};
 	}
 
-	
+	public registerModel<T extends Model>(model: T): boolean {
+		if (model.id in this._registeredModels) {
+			return false;
+		}
+
+		this._registeredModels[model.id] = model;
+		return true;
+	}
+
+	public getModel(id: string): Model | null {
+		if (id in this._registeredModels) {
+			return this._registeredModels[id];
+		}
+
+		return null;
+	}
 }
 
 
