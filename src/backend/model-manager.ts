717d6cc5a4ec0c8751ed3470618e1e35e6b699bd
LoganGerber <logangerber1014@yahoo.com>
2022-04-12 20:33:13 -0700

    Fix import type in model-manager

--- src/backend/model-manager.ts
+++ src/backend/model-manager.ts
@@ -1,4 +1,4 @@
-import { Model } from "./model";
+import Model from "./model";
 
 // TODO: Should this eventually contain a factory for creating models?
 // Model types / names would get registered on launch from plugins.
